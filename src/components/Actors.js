85de564e47b068d415e96f5279e8203859e86da7
mjaber <abujaber44@gmail.com>
2021-03-10 13:41:20 -0500

    commit tree dee0dd082247e5292533dcd89d0f949fdcf2e50c

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { actors } from '../data';
 
+
+
+
 const Actors = () => {
   return (
-    <div>
-      {/*{code here}*/}
-    </div>
+     <div>
+       <h1>Actors Page</h1>
+          {actors.map((actor) =>
+            <div>
+              <h2>Name: {actor.name}</h2>
+              <p>Movies:</p>
+              <ul>{actor.movies.map((movie) => <li>{movie}</li>)}</ul>
+              <br/>
+            </div>
+          )}
+     </div>
   );
 };
 
